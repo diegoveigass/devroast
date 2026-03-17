5adcdcc3f501930ed6717cf0fa37e619e505ca82
Diego Veiga <sdiegoveiga@gmail.com>
2026-03-17 07:47:54 -0300

    feat: add tRPC roast create and result query procedures

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../init";
 import { homeRouter } from "./home";
 import { leaderboardRouter } from "./leaderboard";
+import { roastsRouter } from "./roasts";
 
 export const appRouter = router({
   home: homeRouter,
   leaderboard: leaderboardRouter,
+  roasts: roastsRouter,
 });
 
 export type AppRouter = typeof appRouter;
